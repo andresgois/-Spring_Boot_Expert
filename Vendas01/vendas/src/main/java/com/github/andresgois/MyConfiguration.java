eaa09eaf2f242dd18023ee4b4128b9b0aa2de349
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-25 20:58:04 -0300

    :sparkles: feat: multiplos profiles

--- Vendas01/vendas/src/main/java/com/github/andresgois/MyConfiguration.java
+++ Vendas01/vendas/src/main/java/com/github/andresgois/MyConfiguration.java
@@ -1,11 +1,20 @@
 package com.github.andresgois;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
 
+    @Bean
+    public CommandLineRunner init(){
+        return args -> {
+          System.out.println("================= Development =================");
+        };
+    }
     @Bean("applicationName")
     public String applicationName(){
         return "Sistema de vendas";

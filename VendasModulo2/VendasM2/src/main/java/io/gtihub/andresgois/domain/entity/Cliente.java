4276ecd1c6af404a859aecc9532062177cb9a9bd
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-25 22:35:59 -0300

    :wrong: chore: insert automatico do banco

--- VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/entity/Cliente.java
+++ VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/entity/Cliente.java
@@ -1,2 +1,19 @@
-package io.gtihub.andresgois.domain.entity;public class Cliente {
+package io.gtihub.andresgois.domain.entity;
+
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+
+    public Cliente(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }

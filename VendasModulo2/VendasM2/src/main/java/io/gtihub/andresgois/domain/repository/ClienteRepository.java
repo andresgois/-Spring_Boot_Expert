d173bc59bd1a845b3b0edc1b0fc32f2fde694e53
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-27 18:55:59 -0300

    :sparkles: feat: dados com entity managed

--- VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/repository/ClienteRepository.java
+++ VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/repository/ClienteRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Repository
 public class ClienteRepository {
@@ -18,4 +20,40 @@ public class ClienteRepository {
         entityManager.persist(c);
         return c;
     }
+
+    @Transactional
+    public Cliente atualizar(Cliente c){
+        entityManager.merge(c);
+        return c;
+    }
+
+    @Transactional
+    public void remover(Cliente c){
+        if(!entityManager.contains(c)){
+            c = entityManager.merge(c);
+        }
+        entityManager.remove(c);
+    }
+
+    @Transactional
+    public void remover(Integer id){
+        Cliente cli = entityManager.find(Cliente.class, id);
+        remover(cli);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cliente> buscaPorNome(String nome){
+        String jpql = " select c from cliente c where c.nome like :nome ";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome", "%"+nome+"%");
+        return query.getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cliente> obterTodos(){
+        return entityManager
+                .createQuery(" from cliente", Cliente.class)
+                .getResultList();
+    }
+
 }

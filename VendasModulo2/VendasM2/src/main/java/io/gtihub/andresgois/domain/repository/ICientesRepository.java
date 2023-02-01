909b37e42dc38910dd8b67d4bbb10da492979ce9
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-31 21:36:03 -0300

    :sparkles: feat:Para modificadores: delete e update

--- VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/repository/ICientesRepository.java
+++ VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/repository/ICientesRepository.java
@@ -2,17 +2,31 @@ package io.gtihub.andresgois.domain.repository;
 
 import io.gtihub.andresgois.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ICientesRepository extends JpaRepository<Cliente, Integer> {
 
+    // Query method
     List<Cliente> findByNomeLike(String nome);
     List<Cliente> findByNomeOrId(String nome, Integer id);
-
     Cliente findOneByNome(String nome);
     boolean existsByNome(String nome);
-    // Query method
 
+    // query
+    @Query(value = " select c from Cliente c where c.nome like :nome ")
+    List<Cliente> buscaPorNome(@Param("nome") String nome);
+
+    // query nativa
+    @Query(value = " select * from Cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    List<Cliente> buscaPorNomeNoBanco(@Param("nome") String nome);
+
+    // Para modificadores: delete e update
+    @Query(value = " delete from Cliente c where c.nome = :nome ")
+    @Modifying
+    void deleteByNome(String nome);
 
 }

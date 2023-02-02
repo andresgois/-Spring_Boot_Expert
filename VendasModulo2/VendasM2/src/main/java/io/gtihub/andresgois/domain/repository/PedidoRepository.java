2069258aa2f47513427beb0c98d6629cce53a4a3
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-01 21:28:04 -0300

    :sparkles:	feat: consulta fetch cliente

--- VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/repository/PedidoRepository.java
+++ VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/repository/PedidoRepository.java
@@ -1,7 +1,12 @@
 package io.gtihub.andresgois.domain.repository;
 
+import io.gtihub.andresgois.domain.entity.Cliente;
 import io.gtihub.andresgois.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+    List<Pedido> findByCliente(Cliente c);
 }

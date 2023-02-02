4a45c2229e82eb83b8a0a938366b2978e6f72e34
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-01 21:28:46 -0300

    :bug:	fix: correção valor total

--- VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/entity/Pedido.java
+++ VendasModulo2/VendasM2/src/main/java/io/gtihub/andresgois/domain/entity/Pedido.java
@@ -20,11 +20,13 @@ public class Pedido {
     @Column(name = "data_pedido")
     private LocalDate dataPedido;
 
-    @Column(name = "total", length = 20, precision = 2)
+    @Column(name = "total", precision = 20, scale = 2)
     private BigDecimal total;
 
     @OneToMany(mappedBy = "pedido") // quando não há nenhuma chave na tabela
     private List<ItemPedido> itens;
+
+    public Pedido() {    }
 
     public Pedido(Integer id, Cliente cliente, LocalDate dataPedido, BigDecimal total) {
         this.id = id;
@@ -71,5 +73,14 @@ public class Pedido {
 
     public void setTotal(BigDecimal total) {
         this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + id +
+                ", dataPedido=" + dataPedido +
+                ", total=" + total +
+                '}';
     }
 }

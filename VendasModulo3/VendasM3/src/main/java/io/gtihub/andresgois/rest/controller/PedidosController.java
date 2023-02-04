022d51e80f4d291cd373e0fd48926c551fad5a2b
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-04 16:43:52 -0300

    :sparkles:	feat: controller salvar pedido status

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/rest/controller/PedidosController.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/rest/controller/PedidosController.java
@@ -1,8 +1,10 @@
 package io.gtihub.andresgois.rest.controller;
 
+import io.gtihub.andresgois.domain.entity.Pedido;
+import io.gtihub.andresgois.rest.dto.PedidoDTO;
 import io.gtihub.andresgois.service.PedidoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -14,5 +16,10 @@ public class PedidosController {
         this.service = service;
     }
 
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Integer save(@RequestBody PedidoDTO dto){
+        Pedido pedido = service.salvar(dto);
+        return pedido.getId();
+    }
 }

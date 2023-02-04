56ada0ded57f39473e1b54c597e81011c9499324
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-03 21:21:04 -0300

    :recycle:	refactor: utlizando lombok

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/domain/entity/Pedido.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/domain/entity/Pedido.java
@@ -1,10 +1,17 @@
 package io.gtihub.andresgois.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "pedido")
 public class Pedido {
@@ -26,61 +33,10 @@ public class Pedido {
     @OneToMany(mappedBy = "pedido") // quando não há nenhuma chave na tabela
     private List<ItemPedido> itens;
 
-    public Pedido() {    }
-
-    public Pedido(Integer id, Cliente cliente, LocalDate dataPedido, BigDecimal total) {
-        this.id = id;
-        this.cliente = cliente;
-        this.dataPedido = dataPedido;
-        this.total = total;
-    }
-
-    public List<ItemPedido> getItens() {
-        return itens;
-    }
-
-    public void setItens(List<ItemPedido> itens) {
-        this.itens = itens;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public LocalDate getDataPedido() {
-        return dataPedido;
-    }
-
-    public void setDataPedido(LocalDate dataPedido) {
-        this.dataPedido = dataPedido;
-    }
-
-    public BigDecimal getTotal() {
-        return total;
-    }
-
-    public void setTotal(BigDecimal total) {
-        this.total = total;
-    }
-
-    @Override
-    public String toString() {
-        return "Pedido{" +
-                "id=" + id +
-                ", dataPedido=" + dataPedido +
-                ", total=" + total +
-                '}';
-    }
+    /*public List<ItemPedido> getItens(){
+        if(this.itens == null){
+            this.itens = new ArrayList<>();
+        }
+        return this.itens;
+    }*/
 }

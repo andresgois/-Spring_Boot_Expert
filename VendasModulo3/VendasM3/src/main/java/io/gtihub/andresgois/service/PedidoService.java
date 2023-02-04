1a29034202d56a5905b37922c93f5ea811574f23
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-04 16:41:58 -0300

    :sparkles:	feat: interface do salvar pedido

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/service/PedidoService.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/service/PedidoService.java
@@ -1,6 +1,9 @@
 package io.gtihub.andresgois.service;
 
+import io.gtihub.andresgois.domain.entity.Pedido;
+import io.gtihub.andresgois.rest.dto.PedidoDTO;
+
 public interface PedidoService {
 
-
+    Pedido salvar(PedidoDTO dto);
 }

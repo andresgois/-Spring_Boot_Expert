26a9f63e065266620e0ce33d828f273ecb3eaa6d
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-04 21:40:54 -0300

    :sparkles:	feat: método para buscar por itens

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/domain/repository/PedidoRepository.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/domain/repository/PedidoRepository.java
@@ -3,10 +3,15 @@ package io.gtihub.andresgois.domain.repository;
 import io.gtihub.andresgois.domain.entity.Cliente;
 import io.gtihub.andresgois.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
     List<Pedido> findByCliente(Cliente c);
+
+    @Query(" select p from Pedido p left join fetch p.itens where p.id = :id ")
+    Optional<Pedido> findByFetchItens(Integer id);
 }

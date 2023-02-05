622ff7808d3db8b4de6a514f2347ae1be116d580
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-04 21:41:44 -0300

    :sparkles:	feat: end point busca por pedido

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/rest/controller/PedidosController.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/rest/controller/PedidosController.java
@@ -1,10 +1,20 @@
 package io.gtihub.andresgois.rest.controller;
 
+import io.gtihub.andresgois.domain.entity.ItemPedido;
 import io.gtihub.andresgois.domain.entity.Pedido;
+import io.gtihub.andresgois.rest.dto.InformacoesItemPedidoDTO;
+import io.gtihub.andresgois.rest.dto.InformacoesPedidoDTO;
 import io.gtihub.andresgois.rest.dto.PedidoDTO;
 import io.gtihub.andresgois.service.PedidoService;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -22,4 +32,45 @@ public class PedidosController {
         Pedido pedido = service.salvar(dto);
         return pedido.getId();
     }
+
+    @GetMapping("{id}")
+    public InformacoesPedidoDTO getById(@PathVariable Integer id){
+        return service.obterPedidoCompleto(id)
+                .map( p -> converter(p))
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Pedido não encontrado!")
+                );
+    }
+
+    private InformacoesPedidoDTO converter(Pedido p) {
+        return InformacoesPedidoDTO
+                .builder()
+                .codigo(p.getId())
+                .dataPedido(p.getDataPedido()
+                        .format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+                .cpf(p.getCliente().getCpf())
+                .nomeCliente(p.getCliente().getNome())
+                .total(p.getTotal())
+                .items(converterItens(p.getItens()))
+                .build();
+    }
+
+    private List<InformacoesItemPedidoDTO> converterItens(List<ItemPedido> itens){
+        if(CollectionUtils.isEmpty(itens)){
+            return Collections.emptyList();
+        }
+
+        return itens
+                .stream()
+                .map(
+                        item -> InformacoesItemPedidoDTO
+                                .builder()
+                                .descricaoProduto(item.getProduto().getDescricao())
+                                .precoUnitario(item.getProduto().getPreco())
+                                .quantidade(item.getQuantidade())
+                                .build()
+                ).collect(Collectors.toList());
+    }
+
+
 }

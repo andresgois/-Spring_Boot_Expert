60586f366b8651621dc4e338cc8e2d773b0f5d6f
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-04 21:41:18 -0300

    :sparkles:	feat: obter pedidos completos

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/service/PedidoService.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/service/PedidoService.java
@@ -3,7 +3,10 @@ package io.gtihub.andresgois.service;
 import io.gtihub.andresgois.domain.entity.Pedido;
 import io.gtihub.andresgois.rest.dto.PedidoDTO;
 
+import java.util.Optional;
+
 public interface PedidoService {
 
     Pedido salvar(PedidoDTO dto);
+    Optional<Pedido> obterPedidoCompleto(Integer id);
 }
--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/service/impl/PedidoServiceImpl.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/service/impl/PedidoServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -53,6 +54,11 @@ public class PedidoServiceImpl implements PedidoService {
         pedido.setItens(itemPedido);
 
         return pedido;
+    }
+
+    @Override
+    public Optional<Pedido> obterPedidoCompleto(Integer id) {
+        return pedidoRepository.findByFetchItens(id);
     }
 
     private List<ItemPedido> converterItems(Pedido pedido, List<ItemPedidoDTO> items){

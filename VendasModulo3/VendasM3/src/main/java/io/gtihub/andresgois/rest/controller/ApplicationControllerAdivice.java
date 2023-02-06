456c30debfc24412afcc5dde6206ab6ece1dcb2d
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-06 20:13:40 -0300

    :sparkles:	feat: tratamento de erro para o tipo validação

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/rest/controller/ApplicationControllerAdivice.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/rest/controller/ApplicationControllerAdivice.java
@@ -4,9 +4,13 @@ import io.gtihub.andresgois.exception.PedidoNaoEncontradoException;
 import io.gtihub.andresgois.exception.RegraNegocioException;
 import io.gtihub.andresgois.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdivice {
@@ -22,5 +26,15 @@ public class ApplicationControllerAdivice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrors handlePedidoNotFoundException(PedidoNaoEncontradoException ex){
         return new ApiErrors(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handlePedidoNotValidException(MethodArgumentNotValidException ex){
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map( e -> e.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErrors(errors);
     }
 }

62b4799e4862442c70e545d18c0fe350ed175a7a
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-06 20:24:03 -0300

    :sparkles:	feat: validação do pedido

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/rest/dto/PedidoDTO.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/rest/dto/PedidoDTO.java
@@ -2,6 +2,7 @@ package io.gtihub.andresgois.rest.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -10,7 +11,9 @@ import java.util.List;
 @Data
 public class PedidoDTO {
 
+    @NotNull(message = "Informe o código do cliente.")
     private Integer cliente;
+    @NotNull(message = "Campo total do pedido é obrigatório")
     private BigDecimal total;
     private List<ItemPedidoDTO> items;
 

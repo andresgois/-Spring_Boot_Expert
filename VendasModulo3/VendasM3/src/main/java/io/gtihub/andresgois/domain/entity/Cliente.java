6da0a1a920977228a4eabeb21ed76347ed1b68fa
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-06 20:16:42 -0300

    :sparkles:	feat: validação do cpf

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/domain/entity/Cliente.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/domain/entity/Cliente.java
@@ -1,6 +1,7 @@
 package io.gtihub.andresgois.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
@@ -18,6 +19,8 @@ public class Cliente {
     private String nome;
 
     @Column(name = "cpf", length = 11)
+    @NotEmpty(message = "Campo CPF é obrigatório.")
+    @CPF
     private String cpf;
     @JsonIgnore
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)

0befc9837fa562bda4e802af4645e3732494d9c3
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-09 20:10:40 -0300

    :sparkles: feat: Configurando as Roles e Authorities

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/config/SecurityConfig.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/config/SecurityConfig.java
@@ -32,14 +32,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override  // Método para autorização
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
-
-        http
-                .csrf().disable()
+        // versão 2
+        /*http.csrf().disable()// para aplicação web
                 .authorizeRequests()
-                    .antMatchers("/api/clientes/**")
-                        .authenticated()
+                    .antMatchers("/api/clientes/**")// quem pode acessar o quê
+                        .authenticated() //.roles("USER") //.hasAuthority("MANTER_USUARIO")
                 //.permitAll()
                 .and()
-                .formLogin();
+                .formLogin();*/
+
+        // Versão 3
+        http.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/api/clientes/**").hasAnyRole("ADMIN","USER")
+                    .antMatchers("/api/pedidos/**").hasAnyRole("ADMIN","USER")
+                    .antMatchers("/api/produtos/**").hasRole("ADMIN")
+                .and().formLogin();
     }
 }

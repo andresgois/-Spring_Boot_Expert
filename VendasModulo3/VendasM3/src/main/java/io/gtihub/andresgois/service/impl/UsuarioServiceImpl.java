ace121c9d72620adfc535b8b6762b6c20a25e979
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-02-10 20:53:10 -0300

    :sparkles:	feat: criação do service do usuario

--- VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/service/impl/UsuarioServiceImpl.java
+++ VendasModulo3/VendasM3/src/main/java/io/gtihub/andresgois/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,7 @@
 package io.gtihub.andresgois.service.impl;
 
+import io.gtihub.andresgois.domain.entity.Usuario;
+import io.gtihub.andresgois.domain.repository.IUsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,15 +9,25 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioServiceImpl implements UserDetailsService {
 
     @Autowired
     private PasswordEncoder encoder;
+
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if(!username.equals("cicrano")){
+        /*if(!username.equals("cicrano")){
             throw new UsernameNotFoundException("Usuário não encontrado!");
         }
         return User
@@ -23,6 +35,19 @@ public class UsuarioServiceImpl implements UserDetailsService {
                 .username("cicrano")
                 .password(encoder.encode("123"))
                 .roles("USER","ADMIN")
+                .build();*/
+
+
+        Usuario usuario = usuarioRepository.findByLogin(username).orElseThrow(
+                () -> new UsernameNotFoundException("Usuário não encontrado!"));
+        String[] roles = usuario.isAdmin() ?
+                new String[] {"ADMIN", "USER"} : new String[]{"USER"};
+
+        return User
+                .builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(roles)
                 .build();
     }
 }

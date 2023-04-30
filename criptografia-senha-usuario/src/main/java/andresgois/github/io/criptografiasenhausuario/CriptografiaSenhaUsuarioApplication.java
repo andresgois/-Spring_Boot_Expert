4137f592a6b54f97eb425ce5a5da172966ad1181
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-04-30 13:42:26 -0300

    :heavy_check_mark:	test: teste pra rodar aplicacao

--- criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/CriptografiaSenhaUsuarioApplication.java
+++ criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/CriptografiaSenhaUsuarioApplication.java
@@ -1,10 +1,19 @@
 package andresgois.github.io.criptografiasenhausuario;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CriptografiaSenhaUsuarioApplication {
+
+	@Bean
+	public CommandLineRunner teste(){
+		return args -> {
+			System.out.println("Running");
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CriptografiaSenhaUsuarioApplication.class, args);

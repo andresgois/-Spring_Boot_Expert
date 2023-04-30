44df3d6ce4c0e28d0244b8ae48f7b0967fe3ab04
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-04-30 14:35:23 -0300

    :sparkles:	feat: metodo do brcryp

--- criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/CriptografiaSenhaUsuarioApplication.java
+++ criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/CriptografiaSenhaUsuarioApplication.java
@@ -3,9 +3,12 @@ package andresgois.github.io.criptografiasenhausuario;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class CriptografiaSenhaUsuarioApplication {
 
 	@Bean
@@ -13,6 +16,12 @@ public class CriptografiaSenhaUsuarioApplication {
 		return args -> {
 			System.out.println("Running");
 		};
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEnconder(){
+		BCryptPasswordEncoder enconder = new BCryptPasswordEncoder();
+		return enconder;
 	}
 
 	public static void main(String[] args) {

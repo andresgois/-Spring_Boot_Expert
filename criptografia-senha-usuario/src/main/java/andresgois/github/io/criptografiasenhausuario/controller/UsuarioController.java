b50cda5343d53bcbb05f454f80006c4004ebc6a2
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-04-30 15:00:35 -0300

    :sparkles:	feat: adicioando endpoint para valida senha do usuario

--- criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/controller/UsuarioController.java
+++ criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/controller/UsuarioController.java
@@ -3,10 +3,13 @@ package andresgois.github.io.criptografiasenhausuario.controller;
 import andresgois.github.io.criptografiasenhausuario.model.Usuario;
 import andresgois.github.io.criptografiasenhausuario.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuario")
@@ -15,8 +18,12 @@ public class UsuarioController {
     @Autowired
     private final UsuarioRepository usuarioRepository;
 
-    public UsuarioController(UsuarioRepository usuarioRepository) {
+    @Autowired
+    private final PasswordEncoder encoder;
+
+    public UsuarioController(UsuarioRepository usuarioRepository, PasswordEncoder encoder) {
         this.usuarioRepository = usuarioRepository;
+        this.encoder = encoder;
     }
 
     @GetMapping
@@ -26,7 +33,22 @@ public class UsuarioController {
 
     @PostMapping
     public ResponseEntity<Usuario> salvar(@RequestBody Usuario user){
+        user.setPassword(encoder.encode(user.getPassword()));
         return ResponseEntity.ok(usuarioRepository.save(user));
+    }
+
+    @GetMapping("/validaSenha")
+    public ResponseEntity<Boolean> validaSenha(@RequestParam String login,
+                                                     @RequestParam String password){
+
+        Optional<Usuario> optUser = usuarioRepository.findByLogin(login);
+        if(optUser.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+        Usuario u = optUser.get();
+        boolean valid = encoder.matches(password, u.getPassword());
+        HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+        return ResponseEntity.status(status).body(valid);
     }
 
 }

e296305391d23821cc8cd3e95de0c424f95599e7
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-04-30 14:36:19 -0300

    :recycle: refactor: criando metodo do tipo optinal pra buscar por login

--- criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/repository/UsuarioRepository.java
+++ criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package andresgois.github.io.criptografiasenhausuario.repository;
 
-public interface UsuarioRepository {
+import andresgois.github.io.criptografiasenhausuario.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    public Optional<Usuario> findByLogin(String login);
+
 }

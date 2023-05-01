f92ece6ef2aca481479ff69e8dff260225474291
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-05-01 10:50:42 -0300

    :ok_hand:	style: remocao de espaco

--- criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/repository/UsuarioRepository.java
+++ criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/repository/UsuarioRepository.java
@@ -2,7 +2,6 @@ package andresgois.github.io.criptografiasenhausuario.repository;
 
 import andresgois.github.io.criptografiasenhausuario.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {

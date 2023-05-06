c6485cc8929e6ee8e3b14a43057aeb0c9afba798
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-05-06 12:16:09 -0300

    :sparkles: feat: criar categoria

--- bookstore/src/main/java/br/andresgois/github/io/resource/CategoriaResource.java
+++ bookstore/src/main/java/br/andresgois/github/io/resource/CategoriaResource.java
@@ -1,5 +1,6 @@
 package br.andresgois.github.io.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.andresgois.github.io.domain.Categoria;
 import br.andresgois.github.io.dtos.CategoriaDTO;
@@ -32,5 +36,12 @@ public class CategoriaResource {
         List<Categoria> obj = categoriaService.findAll();
         List<CategoriaDTO> objDto = obj.stream().map(item -> new CategoriaDTO(item)).collect(Collectors.toList());
         return ResponseEntity.ok(objDto);
+    }
+    
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria categoria){
+        Categoria obj = categoriaService.create(categoria);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build(); //.body(new CategoriaDTO(categoria));
     }
 }
--- bookstore/src/main/java/br/andresgois/github/io/service/CategoriaService.java
+++ bookstore/src/main/java/br/andresgois/github/io/service/CategoriaService.java
@@ -25,4 +25,9 @@ public class CategoriaService {
     public List<Categoria> findAll() {
         return categoriaRepository.findAll();
     }
+
+    public Categoria create(Categoria categoria) {
+        categoria.setId(null);
+        return categoriaRepository.save(categoria);
+    }
 }

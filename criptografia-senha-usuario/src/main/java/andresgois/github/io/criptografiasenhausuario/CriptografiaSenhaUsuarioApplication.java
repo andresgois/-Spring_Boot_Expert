be9c1fbfd363873b4717dce685ac809ba4269975
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-07-26 21:35:54 -0300

    ⭐ feat(scope):  adicionando usuario adao

--- criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/CriptografiaSenhaUsuarioApplication.java
+++ criptografia-senha-usuario/src/main/java/andresgois/github/io/criptografiasenhausuario/CriptografiaSenhaUsuarioApplication.java
@@ -1,9 +1,11 @@
 package andresgois.github.io.criptografiasenhausuario;
 
+import andresgois.github.io.criptografiasenhausuario.model.Usuario;
+import andresgois.github.io.criptografiasenhausuario.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,9 +14,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class CriptografiaSenhaUsuarioApplication {
 
-	//@Bean
+	@Autowired
+	private UsuarioRepository repository;
+	@Autowired
+	private PasswordEncoder encoder;
+
+	@Bean
 	public CommandLineRunner teste(){
 		return args -> {
+			Usuario u = new Usuario(null, "andre", encoder.encode("123456"));
+			repository.save(u);
 			System.out.println("Running");
 		};
 	}
